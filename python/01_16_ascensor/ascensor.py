0ddcf0363706200eacdd4e7c0e66e4659f6dc85f
JuanezAlvaro <alvarojuanezgit01@gmail.com>
2023-03-09 12:24:33 -0400

    Programa Ascensor
    uso de condicionales dentro de otras, coondicionales anidadas

---
+++ python/01_16_ascensor/ascensor.py
@@ -0,0 +1,37 @@
+'''
+Programa : 01_16_Ascensor
+El ascensor en este programa consta de 3 plantas:
+    planta baja
+    primer piso
+    segundo piso.
+
+la interacción sera la misma de un ascensor pero si el usuario pide subir a un piso inexistente
+tanto en la subida o bajada el programa devolvera un mensaje de  Error!.
+'''
+
+#Obten los datos necesarios para el programa
+PISO = str(input("Piso? "))
+BOTON = str(input("Boton? "))
+
+#Condiciona es uso del ascensor acorde a los pisos disponibles
+if PISO == "planta baja":   #Planta baja
+    if BOTON == "subir uno":
+        print("primer piso")
+    elif BOTON == "subir dos":
+        print("segundo piso")
+    else:
+        print("Error!")
+elif PISO == "primer piso":     #Primer piso
+    if BOTON == "subir uno":
+        print("segundo piso")
+    elif BOTON == "bajar uno":
+        print("planta baja")
+    else:
+        print("Error!")
+elif PISO == "segundo piso":    #Segundo piso
+    if BOTON == "bajar dos":
+        print("planta baja")
+    elif PISO == "bajar uno":
+        print("primer piso")
+    else:
+        print("Error!")
